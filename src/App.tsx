efa5c9d7d7f5bd0a9bc69e812f0cb7bc5f1c5523
Jim Barnes <jimbarnesdeveloper@gmail.com>
2024-05-01 14:27:02 -0400

    Switched project over to react-map-gl and added first few layers

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,91 @@
-import { useEffect, useRef, useState } from 'react'
-import mapboxgl, { NavigationControl } from 'mapbox-gl';
-import { Map } from 'mapbox-gl';
+import { useState } from 'react'
+import {
+  Map,
+  GeolocateControl,
+  FullscreenControl,
+  NavigationControl,
+  ScaleControl,
+  Source,
+  Layer,
+  SymbolLayer
+} from 'react-map-gl';
 
-import './App.css'
+import {
+  MapIcon
+} from './components/MapImage';
 
-console.log(import.meta.env.VITE_MAPBOX_TOKEN);
+import './App.scss'
 
-mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+const TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
 
 function App() {
-  const mapContainer = useRef<HTMLDivElement>(null);
   const [lng, setLng] = useState(-81.200142);
   const [lat, setLat] = useState(28.602368);
   const [zoom, setZoom] = useState(15);
 
-  useEffect(() => {
-    const map = new Map({
-      container: mapContainer.current!,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
-
-    map.addControl(new NavigationControl(), "top-left");
-
-    return () => map.remove();
-
+  const [visibility, setVisibility] = useState({
+    locations: true,
+    departments: true,
+    emPhones: true
   });
 
+  const locationLayer: SymbolLayer = {
+    id: 'location-layer',
+    type: 'symbol',
+    layout: {
+      "icon-image": 'location',
+      'visibility': visibility.locations! ? "visible" : "none"
+    }
+  };
+
+  const departmentsLayer: SymbolLayer = {
+    id: 'departments-layer',
+    type: 'symbol',
+    layout: {
+      'icon-image': 'building',
+      'visibility': visibility.departments! ? "visible" : "none"
+    }
+  };
+
+  const emPhonesLayer: SymbolLayer = {
+    id: 'emergency-phones-layer',
+    type: 'symbol',
+    layout: {
+      'icon-image': 'phone',
+      'visibility': visibility.emPhones! ? "visible" : "none"
+    }
+  };
+
   return (
-    <>
-      <div ref={mapContainer} className="map-container"></div>
-    </>
+    <div className='map-container'>
+      <Map initialViewState={{
+          latitude: lat,
+          longitude: lng,
+          zoom: zoom
+        }}
+        mapStyle='mapbox://styles/mapbox/streets-v12'
+        mapboxAccessToken={ TOKEN } >
+          <GeolocateControl position="top-left" />
+          <FullscreenControl position="top-left" />
+          <NavigationControl position="top-left" />
+          <ScaleControl />
+          <Source type="geojson" data="/data/geojson/buildingsv2.geojson">
+            <Layer {...locationLayer} />
+          </Source>
+          <Source type="geojson" data="/data/geojson/departments.geojson">
+            <Layer {...departmentsLayer} />
+          </Source>
+          <Source type="geojson" data="/data/geojson/emergency-phones2.geojson">
+            <Layer {...emPhonesLayer} />
+          </Source>
+
+          <MapIcon iconName='location' iconImageSource='/img/location.png' />
+          <MapIcon iconName='building' iconImageSource='/img/building.png' />
+          <MapIcon iconName='food' iconImageSource='/img/food.png' />
+          <MapIcon iconName='handicap' iconImageSource='/img/handicap.png' />
+          <MapIcon iconName='phone' iconImageSource='/img/phone.png' />
+      </Map>
+    </div>
   )
 }
 
---
+++ src/components/MapImage.tsx
@@ -0,0 +1,19 @@
+import { useMap } from 'react-map-gl';
+
+export interface MapIconProps {
+    iconImageSource: string,
+    iconName: string
+}
+
+export function MapIcon(props: MapIconProps) {
+    const { current: map } = useMap();
+
+    if (!map?.hasImage(props.iconName)) {
+        map?.loadImage(props.iconImageSource, (err, img) => {
+            if (err) throw err;
+            if (!map.hasImage(props.iconName)) map.addImage(props.iconName, img!);
+        });
+    }
+
+    return null;
+}

10fea460887276bb00b3126771b9a50ff3233ed4
Ramin Farhadi <ra680091@CMN9J7HFG4MN.local>
2024-06-06 14:34:37 -0400

    size of campus-bar was modified.

--- src/components/Campuses.tsx
+++ src/components/Campuses.tsx
@@ -13,7 +13,7 @@ interface CampusesProps {
 
 const Campuses = ({ campusesData, campusHandler }: CampusesProps) => {
   return (
-    <div className="campus-bar d-flex flex-wrap justify-content-center py-1 bg-secondary">
+    <div className="campus-bar d-flex flex-wrap justify-content-center py-2 bg-secondary">
     { campusesData.map((campusData, index) => (
         <a key={index} onClick={()=> campusHandler(campusData.latitude, campusData.longitude, campusData.zoom)} role="button" className="link-underline-light link-offset-2 link-underline-opacity-100 link-underline-opacity-0-hover ms-3 ms-md-5 text-white">{campusData.name}</a>
       ))}

ca44e9475ab222e416a11653ed775217778282ae
Ramin Farhadi <ra680091@CMN9J7HFG4MN.attlocal.net>
2024-06-14 10:40:21 -0400

    Magnifying glass added to the search bar.

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -12,12 +12,17 @@ export default function SearchResults(props: SearchResultsProps) {
   console.log(props.searchResults);
 
   return (
-    <div className='search-control-wrapper'>
+    <div className='search-control-wrapper rounded'>
+      <div className='input-group'>
       <input
-        className='w-100'
+        className='form-control'
         type='text'
         placeholder='Find locations, services, parking & more... '
         onChange={(e) => props.searchData(e.target.value)} />
+        <div className="input-group-text border-0 bg-black" id="search-addon">
+          <img width={20} src="./img/search-white.png" />
+        </div>
+      </div>  
       {props.searchResults && props.searchResults.length > 0 && (
         <div className='search-results-container'>
           <h2 className='sr-only'>Search Results</h2>

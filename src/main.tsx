57571f28b722a38886c1aaaf974df2621801de16
Jim Barnes <jimbarnesdeveloper@gmail.com>
2024-06-27 13:44:45 -0400

    Updated application to take advantage of the react router

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.scss'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <App />
+    )
+  }
+], {
+  basename: import.meta.env.BASE_URL
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )

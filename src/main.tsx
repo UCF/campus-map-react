227076afeeaf452be72e9820a97ce3c928f2674e
Ramin Farhadi <ra680091@CMN9J7HFG4MN.attlocal.net>
2024-07-01 13:50:35 -0400

    debugging.

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <App />
   </React.StrictMode>
 )
